9da5e58c914cb4f89fc70def2183fe7503036ebc
pleijan <pierre.tom@outlook.com>
2020-10-17 15:22:13 +0000

    création de la maquette (a confirmer)

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+echo"<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=\"UTF-8\">
+  <title>Technologie</title>
+  <link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" />
+<div id=\"blocmenu\"> 
+      <ul id=\"menu\"> 
+        <li><a class=\"active\" href=\"index.html\">&#127968; Accueil</a></li>
+        <li><a href=\"portable.html\">Fonc 1</a></li>
+        <li><a href=\"ordinateur.html\">Fonc 2</a></li>
+        <li><a href=\"console.html\">Fonc 3</a></li>
+        <li><a href=\"tv.html\">Fonc 4</a></li>
+        <li><a href=\"internet.html\">Fonc 5</a></li>
+      </ul>
+</div>
+</head>
+
+<body>
+
+<div id=\"global\"> 
+  <div id=\"entete\">   
+      </br>
+      </br>
+      </br>
+      </br>
+      <h1><p>PRO - RESEAU</p></h1>
+      <HR width=1240>
+       <h3><p>ici vous pourrez réviser tout les modules de l'INM2013 : réseau</p></h3>
+      <p>
+      <HR width=1240>
+  </div> 
+  
+
+  <div id=\"contenu\">  
+  </div>   
+</div> 
+</body>
+<footer>
+</footer>
+</html>";
+
+?>
