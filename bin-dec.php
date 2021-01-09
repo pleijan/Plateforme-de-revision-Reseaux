a513d4066881c27eb3cf2e8eee599dc0fd7c445c
Tom Pierre <pierre.tom@outlook.com>
2021-01-09 10:50:18 +0100

    fix barre

--- bin-dec.php
+++ bin-dec.php
@@ -1,20 +1,24 @@
-<head>
+<head>  <!-- cette section "head" contient tout ce qui s'occupe des feuilles de style  -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta charset="UTF-8">
   <title>Technologie</title>
   <link rel="stylesheet" href="style.css" type="text/css" />
-<div id="blocmenu"> 
-      <ul id="menu"> 
-        <li><a class='active' href="index.php"><img src='img\logo.png' width='90px'/></a></li>
-        <li><a href="bin-dec.php">conversion adresse ip<br>binaire -> decimal</a></li>
-        <li><a href="hex-dec.php">conversion adresse ip<br>hexadecimal -> decimal</a></li>
-        <li><a href="sousReseaux.php">création de sous réseaux a<br>partir d'une adresse ip</a></li>
-        <li><a href="index.php">Calculer un CRC<br>de type Ethernet</a></li>
-        <li><a href="index.php">Proposer un sniffer<br>nmap</a></li>
-        <li><a href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a></li>
-      </ul>
-</div>
+
 </head>
-<body>
+
+<body> <!-- cette section "body" contientla barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
+
+<table id="blocmenu" align="center" > 
+  <tr id="menu"> 
+    <td><a class='active' href="index.php"><img src='img\logo.png' width='90px'/></a></td>
+    <td><a href="bin-dec.php">conversion adresse ip<br>binaire -> decimal</a></td>
+    <td><a href="hex-dec.php">conversion adresse ip<br>hexadecimal -> decimal</a></td>
+    <td><a href="sousReseaux.php">création de sous réseaux a<br>partir d'une adresse ip</a></td>
+    <td><a href="index.php">Calculer un CRC<br>de type Ethernet</a></td>
+    <td><a href="index.php">Proposer un sniffer<br>nmap</a></td>
+    <td><a href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a></td>
+  </tr>
+</table>
 
 <?php
 
--- index.php
+++ index.php
@@ -2,34 +2,35 @@
 
 <!DOCTYPE html>
 <html>
-<head>
+<head>  <!-- cette section "head" contient tout ce qui s'occupe des feuilles de style  -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta charset="UTF-8">
   <title>Technologie</title>
   <link rel="stylesheet" href="style.css" type="text/css" />
-<div id="blocmenu"> 
-      <ul id="menu"> 
-        <li><a class='active' href="index.php"><img src='img\logo.png' width='90px'/></a></li>
-        <li><a href="bin-dec.php">conversion adresse ip<br>binaire -> decimal</a></li>
-        <li><a href="hex-dec.php">conversion adresse ip<br>hexadecimal -> decimal</a></li>
-        <li><a href="sousReseaux.php">création de sous réseaux a<br>partir d'une adresse ip</a></li>
-        <li><a href="index.php">Calculer un CRC<br>de type Ethernet</a></li>
-        <li><a href="index.php">Proposer un sniffer<br>nmap</a></li>
-        <li><a href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a></li>
-      </ul>
-</div>
+
 </head>
 
-<body>
+<body> <!-- cette section "body" contientla barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
+
+<table id="blocmenu" align="center" > 
+  <tr id="menu"> 
+    <td><a class='active' href="index.php"><img src='img\logo.png' width='90px'/></a></td>
+    <td><a href="bin-dec.php">conversion adresse ip<br>binaire -> decimal</a></td>
+    <td><a href="hex-dec.php">conversion adresse ip<br>hexadecimal -> decimal</a></td>
+    <td><a href="sousReseaux.php">création de sous réseaux a<br>partir d'une adresse ip</a></td>
+    <td><a href="index.php">Calculer un CRC<br>de type Ethernet</a></td>
+    <td><a href="index.php">Proposer un sniffer<br>nmap</a></td>
+    <td><a href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a></td>
+  </tr>
+</table>
 
 <div id="global"> 
   <div id="entete">   
-      <h1><p>titre du site</p></h1>
-      <HR width=1240>
-      <h3><p>ici vous pourrez réviser tout les modules de l'INM2013 : réseau</p></h3>
-      <p>
-      <HR width=1240>
-
-      
+    <h1><p>titre du site</p></h1>
+    <HR width=80%>
+    <h3><p>ici vous pourrez réviser tout les modules de l'INM2013 : réseau</p></h3>
+    <p>
+    <HR width=80%>
   </div> 
   
 
