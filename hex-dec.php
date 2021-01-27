d366c846e878ec58613336d6b6b76f0be2d62d1d
Raffaele G <raffaelejunior@hotmail.fr>
2021-01-27 09:50:53 +0000

    Replace hex-dec.php +CSS

--- hex-dec.php
+++ hex-dec.php
@@ -2,51 +2,80 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta charset="UTF-8">
   <title>Technologie</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="style.css" type="text/css" />
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
-<body> <!-- cette section "body" contientla barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
+<body> <!-- cette section "body" contient la barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
 
-<table id="blocmenu" align="center" > 
-  <tr id="menu"> 
-    <td><a class='active' href="index.php"><img src='img\logo.png' width='90px'/></a></td>
-    <td><a href="bin-dec.php">conversion adresse ip<br>binaire -> decimal</a></td>
-    <td><a href="hex-dec.php">conversion adresse ip<br>hexadecimal -> decimal</a></td>
-    <td><a href="sousReseaux.php">création de sous réseaux a<br>partir d'une adresse ip</a></td>
-    <td><a href="index.php">Calculer un CRC<br>de type Ethernet</a></td>
-    <td><a href="index.php">Proposer un sniffer<br>nmap</a></td>
-    <td><a href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a></td>
-  </tr>
-</table>
 
-<?php
-echo"
-<h2 align='center'><p>Sur cette page vous pourrez apprendre à traduire une adresse IPV4 de hexadecimal à decimal et inversement.</p></h2>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="index.php"><img src='img\logo.jpg' width='260px'/></a>
+
+  
+  <ul class="navbar-nav">
+   
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle active" href="#" id="navbardrop" data-toggle="dropdown">
+        Conversions Adresses IP
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="bin-dec.php">Binaire -> Décimal</a>
+        <a class="dropdown-item" href="hex-dec.php">Hexadecimal -> Décimal</a>
+      </div>
+    </li>
+
+    <!-- Links -->
+     <li class="nav-item">
+      <a class="nav-link" href="sousReseaux.php">Division de réseaux</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Calculer un CRC<br>de type Ethernet</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Proposer un sniffer<br>nmap</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a>
+    </li>
+
+  </ul>
+</nav>
+
+
+<h5 align='center'><p>Sur cette page vous pourrez apprendre à traduire une adresse IPV4 de hexadecimal à decimal et inversement.</p></h5>
 <HR width=1240>
-    <h1 align='center'>Cours</h1>
+    <h3 align='center'><b>Cours</b></h3>
 <HR width=1240>
 
 <p> explication de comment ça marche </p>
 
 <HR width=1240>
-    <h1 align='center'>Application</h1>
+    <h3 align='center'><b>Application</b></h3>
 <HR width=1240>
 
 <form action='hex-dec.php' method='post' >
 <table cellpading='4' cellspacing='4' align='center'>
 
-<tr><td>adresse ip: </td><td><input name='part1' type='text'> .</td>
-<td><input name='part2' type='text'> .</td>
-<td><input name='part3' type='text'> .</td>
-<td><input name='part4' type='text'> </td></tr></table>
+<tr><td>Adresse IP: </td>
+<td><input class='champ' name='part1' type='text' value= "<?php if (isset($_POST['part1'])){echo $_POST['part1'];}?>" required> .</td>
+<td><input class='champ' name='part2' type='text' value= "<?php if (isset($_POST['part2'])){echo $_POST['part2'];}?>" required> .</td>
+<td><input class='champ' name='part3' type='text' value= "<?php if (isset($_POST['part3'])){echo $_POST['part3'];}?>" required> .</td>
+<td><input class='champ' name='part4' type='text' value= "<?php if (isset($_POST['part4'])){echo $_POST['part4'];}?>" required> </td></tr></table>
 
-<table cellpading='4' cellspacing='4' align='center' border='1'>
-<tr><td>hexa -> decimal <input type='radio' name='trad' value='hextodec' checked></td><td>decimal -> hexa<input type='radio' name='trad' value='dectohex'></td></tr>
-<tr><td><input name='valider' type='submit' value='Valider' style='width:100%;height:100%'></td><td><input name='rst' type='reset' value='Annuler' style='width:100%;height:100%'></td></tr></table>
+<table cellpading='4' cellspacing='4' align='center' >
+<tr><td>Hexa -> Décimal <input type='radio' name='trad' value='hextodec' checked></td><td>Décimal -> Hexa<input type='radio' name='trad' value='dectohex'></td></tr>
+</table>
+<input name='valider' class="btn btn-success btn-sm" type='submit' value='Valider' >
 
-</form>";
+</form>
 
+<?php
 if(isset($_POST['part1'],$_POST['part2'],$_POST['part3'],$_POST['part4'])){
     if($_POST['trad']=='dectohex'){
         foreach ($_POST as $val){
@@ -165,3 +194,12 @@ if (isset($_GET['id'])){
 
 
 </body>
+<footer>
+  <HR width=1240>
+   </br>
+   </br>
+  <p id = "copyright"><span id="Copyright symbol">&copy Copyright 2021. IUT de Vélizy - PIERRE TOM - GIANNICO Raffaele - MANOHARAN Anushan - PARISOT Théo. Tous droits r&eacute;serv&eacute;s.</span></p>
+   </br>
+   </br>
+  <HR width=1240>
+</footer>
