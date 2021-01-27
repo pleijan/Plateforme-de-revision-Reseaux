278390120f11b9bc6aa656365691cbea0637d703
Raffaele G <raffaelejunior@hotmail.fr>
2021-01-27 19:32:51 +0000

    Replace index.php

--- index.php
+++ index.php
@@ -57,6 +57,7 @@
   <div id="entete">
 
     <h1><p>titre du site</p></h1>
+    <img src="img/banniere.jpg" class="rounded banniere" alt="banniere">
     <HR width=80%>
 
     <h3><p>ici vous pourrez réviser tout les modules de l'INM2013 : réseau</p></h3>
@@ -66,39 +67,31 @@
   
 
   <div id="contenu">  
-  <h2> Pour tout ce qui concerne les traduction d'adresse ip :</h2>
+  <h2 id = "adresseIP"> Traductions d'adresses IP </h2>
   
   <table align='center'> 
  
-    <tr><td><a href="bin-dec.php">conversion adresse ip binaire -> decimal</a></td></tr>
-    <tr><td><a href="hex-dec.php">conversion adresse ip hexadecimal -> decimal</a></td></tr>
+    <tr><td><a href="bin-dec.php"><img src="img/bin-dec.jpg" class="rounded imgGauche" alt="bin-dec"></a></td>
+    <td><a href="hex-dec.php"><img src="img/hex-dec.jpg"  class="rounded imgDroite" alt="hex-dec"></a></td></tr>
+    <tr><td><br/></td></tr>
+    <tr><td><h2> Division de réseaux</h2></td></tr>
+    
+    <tr><td><a href="sousReseaux.php"><img src="img/cidr.png"  class="rounded imgGauche" alt="cidr"></a></td>
+    <td><a href="hex-dec.php"><img src="img/vlsm.jpg"  class="rounded imgDroite" alt="vlsm"></a></td></tr>
 
-  </table>
-
-  <h2> Pour tout ce qui concerne les sous réseaux d'adresse ip :</h2>
-  
-  <table align='center'> 
+  <tr><td><h2> Sniffer Nmap, CRC</h2></td></tr>
  
-    <tr><td><a href="sousReseaux.php">création de sous réseaux a partir d'une adresse ip</a></td></tr>
+    <tr><td><a href="index.php"><img src="img/nmap.png"  class="rounded imgGauche" alt="nmap"></a></td>
+    <td><a href="index.php"><img src="img/crc.png"  class="rounded imgDroite" alt="crc"></a></td></tr>
 
-  </table>
 
-  <h2> Pour tout ce qui concerne les CRC ou les sniffer de nmap :</h2>
+  <tr><td><h2> Trouver l'adresse IP </h2></td></tr>
   
-  <table align='center'> 
+</table>
  
-    <tr><td><a href="index.php">Calculer un CRC de type Ethernet</a></td></tr>
-    <tr><td><a href="index.php">Proposer un sniffer nmap</a></td></tr>
+  <a href="index.php"><img src="img/trouver-adresse.jpg"  class="rounded imglongue" alt="trouver-adresse"></a>
 
-  </table>
-
-  <h2> Pour trouver l'adresse ip d'une autre machine de votre réseaux local :</h2>
   
-  <table align='center'> 
- 
-    <tr><td><a href="index.php">Trouver l’adresse IP d’une machine extérieure</a></td></tr>
-
-  </table>
 
 
   </div>   
