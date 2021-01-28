bbe93f789495604e05b6f317e4f97b52c06ece8b
Tom Pierre <pierre.tom@outlook.com>
2021-01-28 10:25:46 +0100

    ajout fichier barre de menu qui contient le menu

--- index.php
+++ index.php
@@ -2,60 +2,15 @@
 
 <!DOCTYPE html>
 <html>
-<head>  <!-- cette section "head" contient tout ce qui s'occupe des feuilles de style  -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta charset="UTF-8">
-  <title>Technologie</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css" type="text/css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
 
-<body> <!-- cette section "body" contient la barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
-
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand -->
-   <a class="navbar-brand" href="index.php"><img src='img\logo.jpg' width='260px'/></a>
-
-  
-  <ul class="navbar-nav">
-   
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Conversions Adresses IP
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="bin-dec.php">Binaire -> Décimal</a>
-        <a class="dropdown-item" href="hex-dec.php">Hexadecimal -> Décimal</a>
-      </div>
-    </li>
-
-    <!-- Links -->
-     <li class="nav-item">
-      <a class="nav-link" href="sousReseaux.php">Division de réseaux</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Calculer un CRC<br>de type Ethernet</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Proposer un sniffer<br>nmap</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a>
-    </li>
-
-  </ul>
-</nav>
+<?php
+require("barreDeMenu.php");
+?>
 
 <br/>
 
 <div id="global"> 
   <div id="entete">
-
     <h1><p>titre du site</p></h1>
     <img src="img/banniere.jpg" class="rounded banniere" alt="banniere">
     <HR width=80%>
--- sousReseaux.php
+++ sousReseaux.php
@@ -1,51 +1,6 @@
-<head>  <!-- cette section "head" contient tout ce qui s'occupe des feuilles de style  -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta charset="UTF-8">
-  <title>Technologie</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style.css" type="text/css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-
-<body> <!-- cette section "body" contient la barre de navigation en haut de la page ainsi que tout contenu de la page avec d'abord un entete et en suite le reel contenu -->
-
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand -->
-   <a class="navbar-brand" href="index.php"><img src='img\logo.jpg' width='260px'/></a>
-
-  
-  <ul class="navbar-nav">
-   
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Conversions Adresses IP
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="bin-dec.php">Binaire -> Décimal</a>
-        <a class="dropdown-item" href="hex-dec.php">Hexadecimal -> Décimal</a>
-      </div>
-    </li>
-
-    <!-- Links -->
-     <li class="nav-item">
-      <a class="nav-link active" href="sousReseaux.php">Division de réseaux</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Calculer un CRC<br>de type Ethernet</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Proposer un sniffer<br>nmap</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Trouver l’adresse IP d’une<br>machine extérieure</a>
-    </li>
-
-  </ul>
-</nav>
+<?php
+require("barreDeMenu.php");
+?>
 
 <br/>
 <h5>Sur cette page vous pourrez apprendre à partitionner votre réseau en different sous réseau.</p></h5>
