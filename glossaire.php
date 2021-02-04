79ae464651da20d91067e4f644eb6bd7aba3525c
Tom Pierre <pierre.tom@outlook.com>
2021-02-04 19:43:51 +0100

    ajustement glossaire

--- glossaire.php
+++ glossaire.php
@@ -15,11 +15,14 @@ require("barreDeMenu.php");
 $row = 1;
 if (($handle = fopen("cours/glossaire.csv", "r")) !== FALSE) {
     echo "<table cellpading='10' cellspacing='10' align='center' border ='2'>";
+    $data = fgetcsv($handle, 10000, ";");
+    echo "<tr><td><h4 align='center'><u>$data[0]</u></h4></td><td><h4 align='center'><u>$data[1]</u></h4></td></tr>";
     while (($data = fgetcsv($handle, 10000, ";")) !== FALSE) {
         $num = count($data);
         $row++;
         echo "<tr><td><p align='center'>$data[0]</p></td><td><p align='center'>$data[1]</p></td></tr>";
     }
+    echo"</table>";
     fclose($handle);
 }
 
