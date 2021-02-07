c32b52df65b0cf6dda8a5125cda6b33510e1b290
Raffaele G <raffaelejunior@hotmail.fr>
2021-02-07 13:47:01 +0000

    Replace actionSupprGloss.php

--- actionSupprGloss.php
+++ actionSupprGloss.php
@@ -4,176 +4,76 @@
 if (isset($_POST['newdef']) and isset($_POST['confirmer']) and isset($_POST['motAModif'])){
     $newdef = $_POST['newdef'];
     $motAModif = $_POST['motAModif'];
-    /*********************Recuperation de la ligne concerner************************/
-    $file="cours/glossaire.csv";
-    $fp=fopen($file,"r") or die("impossible de créer le fichier");
+    $row = 1;
+    $toutLeFichier[]="";
+    echo "entrer";
 
-    $continuer=0;
-    while ($ligne = fgetcsv($fp,";")) {
-        if($continuer==0)
-            $ligneString=$ligne;
-        $toutLeFichier[]=$ligne;
-        foreach ($ligne as $mot) {
-            
-            if(strpos($mot, $motAModif) !== FALSE){
-               $continuer=1;
+    $file="cours/glossaire.csv";
+    $fp=fopen($file,"a+") or die("impossible de créer le fichier");
+
+        echo " dans le if";
+        echo "<table cellpading='10' cellspacing='10' align='center' border ='2'>
+        <tr>
+        <td><h4 align='center'><u>Mots</u></h4></td>
+        <td colspan=3><h4 align='center'><u>Définition</u></h4></td>";
+        echo"</table>";
+        while ($data = fgetcsv($fp, 10000, ";")) {
+            $num = count($data);
+            $row++;
+            echo "\n et meme dans la boucle";
+            if(!(strpos($motAModif, $data[0]) !== FALSE)){
+               $toutLeFichier[] = "$data[0];$data[1]\r\n";
+                echo "oui";
             }
         }
-    }
-    fclose($fp);
-    echo $motAModif;
-    echo"ligneString =";
-    print_r($ligneString);
-    echo"<br/>toutLeFichier =";
-    print_r($toutLeFichier);
-        /*******************************************************************************/
-
-    /*********************modification de la ligne concerner************************/
-
-    $ligneString[1]=$newdef;//Modification du mdp
-
-    for($i=0;$i<count($toutLeFichier);$i++){
-        for($j=0;$j<count($toutLeFichier[$i]);$j++){
-            if(strpos($toutLeFichier[$i][$j], $motAModif) !== FALSE){
-                $toutLeFichier[$i]=$ligneString;
-            }
         
+
+        print_r($toutLeFichier);
+        file_put_contents('cours/glossaire.csv', "");
+        foreach ($toutLeFichier as $val) {
+            echo $val;
+            file_put_contents('cours/glossaire.csv', $val, FILE_APPEND);
+
         }
-    }
-    echo"<br/>toutLeFichier =";
-    print_r($toutLeFichier);
-
-    $update = implode(";", $ligneString);
-    $file="cours/glossaire.csv";
-
-    $fileRead = fopen($file, 'r');
-    $fileWrite = fopen($file.'.tmp', 'w');
-
-    if (!$fileRead || !$fileWrite) {
-        echo "Erreur d'ouverture du fichier de lecture et/ou d'écriture avec $filename.";
-    }
-
-    for($i=0;$i<count($toutLeFichier);$i++){
-        fputcsv($fileWrite, $toutLeFichier[$i] ,$delimiter=';');
-    }
-
-
-    fclose($fileWrite);
-    fclose($fileRead);
-    unlink($file);
-    rename($file.'.tmp', $file);
-
+        file_put_contents('cours/glossaire.csv', "$motAModif;$newdef", FILE_APPEND);   
+        fclose($fp);
+    
+    
+   
 }
 
 if (isset($_GET['suppr']))
 {
     $motAModif = $_GET['suppr'];
 
-    /*********************Recuperation des comptes************************/
     $file="cours/glossaire.csv";
-    $fp=fopen($file,"r") or die("impossible de créer le fichier");
+    $fp=fopen($file,"a+") or die("impossible de créer le fichier");
 
-    $continuer=0;
-    while ($ligne = fgetcsv($fp,";")) {
-        
-        
-        foreach ($ligne as $mot) {
-            
-            if(strpos($mot, $motAModif) !== FALSE){
-                $continuer=1;
-            }
+    echo " dans le if";
+    echo "<table cellpading='10' cellspacing='10' align='center' border ='2'>
+    <tr>
+    <td><h4 align='center'><u>Mots</u></h4></td>
+    <td colspan=3><h4 align='center'><u>Définition</u></h4></td>";
+    echo"</table>";
+    while ($data = fgetcsv($fp, 10000, ";")) {
+        $num = count($data);
+        $row++;
+        echo "\n et meme dans la boucle";
+        if(!(strpos($motAModif, $data[0]) !== FALSE)){
+           $toutLeFichier[] = "$data[0];$data[1]\r\n";
+            echo "oui";
         }
-        if($continuer==0)
-            $toutLeFichier[]=$ligne;
-        else
-                $continuer=0;
     }
-    fclose($fp);
-    echo"<br/>toutLeFichier =";
+    
+    
     print_r($toutLeFichier);
-    
-    /*******************************************************************************/
+    file_put_contents('cours/glossaire.csv', "");
+    foreach ($toutLeFichier as $val) {
+        echo $val;
+        file_put_contents('cours/glossaire.csv', $val, FILE_APPEND);
 
-    /*********************modification des comptes************************/
-
-
-    $file="cours/glossaire.csv";
-
-    $fileRead = fopen($file, 'r');
-    $fileWrite = fopen($file.'.tmp', 'w');
-
-    if (!$fileRead || !$fileWrite) {
-        echo "Erreur d'ouverture du fichier de lecture et/ou d'écriture avec $filename.";
-    }
-
-    for($i=0;$i<count($toutLeFichier);$i++){
-        fputcsv($fileWrite, $toutLeFichier[$i] ,$delimiter=';');
-    }
-    
-
-    fclose($fileWrite);
-    fclose($fileRead);
-    unlink($file);
-    rename($file.'.tmp', $file);
-    
-    
-    /*******************************************************************************/
-
-
-
-    /*********************Recuperation des données  ************************/
-    $file="cours/glossaire.csv";
-    $fp=fopen($file,"r") or die("impossible de créer le fichier");
-
-    $continuer=0;
-    while ($ligne = fgetcsv($fp,";")) {
-        
-        
-        foreach ($ligne as $mot) {
-            
-            if(strpos($mot, $motAModif) !== FALSE){
-                $continuer=1;
-            }
-        }
-        if($continuer==0)
-            $toutLeFichier2[]=$ligne;
-        else
-                $continuer=0;
-            
     }
     fclose($fp);
-    echo"<br/>toutLeFichier =";
-    print_r($toutLeFichier2);
-    
-    /*******************************************************************************/
-
-    /*********************modification des données ************************/
-
-
-    $file="data/donnee.csv";
-
-    $fileRead = fopen($file, 'r');
-    $fileWrite = fopen($file.'.tmp', 'w');
-
-    if (!$fileRead || !$fileWrite) {
-        echo "Erreur d'ouverture du fichier de lecture et/ou d'écriture avec $filename.";
-    }
-
-    for($i=0;$i<count($toutLeFichier);$i++){
-        fputcsv($fileWrite, $toutLeFichier2[$i] ,$delimiter=';');
-    }
-    
-
-    fclose($fileWrite);
-    fclose($fileRead);
-    unlink($file);
-    rename($file.'.tmp', $file);
-    
-    
-    /*******************************************************************************/
-
-    $file="cours/glossaire.csv";
-    unlink($file);
 
    
         
