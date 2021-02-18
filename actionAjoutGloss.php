554ac73a6ac3865038e1afefa14b0fae11831369
Tom Pierre <pierre.tom@outlook.com>
2021-02-18 21:58:29 +0100

    tri glossaire

--- actionAjoutGloss.php
+++ actionAjoutGloss.php
@@ -1,35 +1,45 @@
 <?php
+
+function ajout_glossaire($tuple){
+	$fp1=fopen("cours/glossaire.csv", "r");
+	$fp2=fopen("cours/temp.csv","w");
+	if($fp1!==FALSE){	
+		$line=fgetcsv($fp1, 1024,";");
+		$i=1;
+		while (!feof($fp1)&&$i==1) {
+			
+			$comparaison=strnatcmp(ucfirst($tuple[0]),ucfirst($line[0]));
+			if ($comparaison>0){
+				fputcsv($fp2,array($line[0],$line[1]),";");
+			}
+			else if ($comparaison<0){
+				fputcsv($fp2,array($tuple[0],$tuple[1]),";");
+				fputcsv($fp2,array($line[0],$line[1]),";");
+				$i=0;
+			}
+			else return;
+			
+			$line=fgetcsv($fp1, 1024,";");
+		
+		}
+		while (!feof($fp1)) {
+			fputcsv($fp2,array($line[0],$line[1]),";");
+			$line=fgetcsv($fp1, 1024,";");
+		}
+		fclose($fp1);
+		fclose($fp2);
+		unlink("cours/glossaire.csv");
+		rename("cours/temp.csv","cours/glossaire.csv");
+	}
+}
+
 session_start();
-    if(isset($_SESSION['id']) and isset($_POST['mot']) and isset($_POST['def']))
+    if(isset($_SESSION['id']) and isset($_POST['mot']) and isset($_POST['def']) and $_POST['mot']!="" and $_POST['def']!="")
     {
-
-        foreach($_POST as $k => $v){
-        $$k=$v;
-        }
-
-    
-        $file = file_get_contents('cours/glossaire.csv');
-        $header = "$mot;$def\r\n";
-        file_put_contents('cours/glossaire.csv',$header.$file);
-        
-        if (($handle = fopen($file, "rb")) !== FALSE) {
-            // recuperation des lignes dans un array
-            while (($ligne = fgetcsv($handle, 1024, ";")) !== FALSE) {
-                $lignes[] = $ligne;
-            }
-            // tri du plus petit au plus grand
-            sort($lignes);
-        }
-
-        $i = 0;
-        $chaine="";
-        for ($i=0;$i<count($lignes);$i++){
-            $chaine=$chaine+$lignes[$i];
-        }
-
-        file_put_contents('cours/glossaire2.csv',$chaine);
-
-        fclose($handle);
-        
+		
+		$a=array($_POST['mot'],$_POST['def']);
+		ajout_glossaire($a);
     }header('Location: glossaire.php?err=1');
+	
+	
 ?>
