edc9cb6d9f333b58e7d7c0f3de5e70296bc0f7f8
Tom Pierre <pierre.tom@outlook.com>
2021-02-25 10:17:37 +0100

    ajout lien crc au  menu

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ require("barreDeMenu.php");
 
   <table align='center' cellpadding="2"> 
     <tr><td><a href="index.php"><img src="img/nmap.png"  class="rounded imgGauche" alt="nmap" title="Sniffer Nmap"></a></td>
-    <td><a href="index.php"><img src="img/crc.png"  class="rounded imgDroite" alt="crc" title="Calcul de CRC de type Ethernet"></a></td></tr>
+    <td><a href="crc.php"><img src="img/crc.png"  class="rounded imgDroite" alt="crc" title="Calcul de CRC de type Ethernet"></a></td></tr>
   </table>
 
     <h2><HR width=50%>Trouver l'adresse IP <HR width=50%></h2>
