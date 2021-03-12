3354c840660c02bdf2ab1279268c8c035196d3f2
Tom Pierre <pierre.tom@outlook.com>
2021-03-12 16:28:18 +0100

    add ping

---
+++ ping.php
@@ -0,0 +1,30 @@
+
+<?php
+require("barreDeMenu.php");
+?>
+
+<br/>
+
+<h5 align='center'><p>Sur cette page vous pourrez effectuer un ping sur l'adresse que vous souhaiter.</p></h5>
+<HR width=1240>
+    <h3 align='center'><b>Formulaire</b></h3>
+<HR width=1240>
+
+<form action='' method='POST'>
+<table cellpading='4' cellspacing='4' align='center'>
+
+<tr>
+<td>Adresse a ping</td><td><input type='text' name='ping' required></td>
+</tr>
+<tr>
+<td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' ></td><td><input name='valider' class="btn btn-danger btn-sm"type='submit' value='Valider' ></td>
+</tr>
+
+</table>
+</form>
+
+<HR width=1240>
+    <h3 align='center'><b>Affichage</b></h3>
+<HR width=1240>
+
+
