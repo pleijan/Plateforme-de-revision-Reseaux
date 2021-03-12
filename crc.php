97fa3c6924e1b466819ea5d8e2718757c9defe0c
Tom Pierre <pierre.tom@outlook.com>
2021-03-12 15:32:06 +0100

    CRC

--- crc.php
+++ crc.php
@@ -1,5 +1,34 @@
 <?php
 require('barreDeMenu.php');
+
+if(isset($_GET['Message2']) and isset($_GET['polynome']) ){
+    $message2 = $_GET['Message2'];
+    $polynome = $_GET['polynome'];
+    $tailleM2 = strlen($message2);
+    $taillePol = strlen($polynome);
+
+    $arr0 = str_split($message2);
+    foreach ($arr0 as $x) {
+        if (!($x == 0 || $x == 1)) {
+            header('Location:crc.php?id=2');
+            
+        }
+    }
+
+    if($tailleM2>10)header('Location:crc.php?id=5');
+
+
+    $polArray = str_split($polynome);
+    foreach ($polArray as $x) {
+        if (!($x == 0 || $x == 1)) {
+            header('Location:crc.php?id=3');
+            
+        }
+    }
+
+    if($taillePol>$tailleM2) header('Location:crc.php?id=4');
+}
+
 ?>
 
 </br>
@@ -37,6 +66,9 @@ require('barreDeMenu.php');
 <?php
 
     if(isset($_GET['id'])){
+        if($_GET['id']=='0'){
+            echo"<p style='color:red'>message trop grand (max 10 bit) </p>";
+        }
         if($_GET['id']=='1'){
             echo"<p style='color:red'>message incorrect (que du bianire !) </p>";
         }
@@ -46,6 +78,16 @@ require('barreDeMenu.php');
     if(isset($_GET['Message'])){
         $Message = $_GET['Message'];
         $TailleM = strlen($Message);
+
+        $arr0 = str_split($Message);
+        foreach ($arr0 as $x) {
+            if (!($x == 0 || $x == 1)) {
+                header('Location:crc.php?id=1');
+            }
+        }
+
+        if($TailleM>10)header('Location:crc.php?id=0');
+
         echo"<h6><b>quelle taille de matrice voulez-vous :</b></h6>
         
         <form action='' method='get'>
@@ -67,12 +109,7 @@ require('barreDeMenu.php');
         echo"<h6><b>Le message est : $Message</b></h6>";
 
 
-        $arr0 = str_split($Message);
-        foreach ($arr0 as $x) {
-            if (!($x == 0 || $x == 1)) {
-                header('Location:crc.php?id=1');
-            }
-        }
+        
 
 
         $TailleM = strlen($Message);
@@ -137,12 +174,113 @@ require('barreDeMenu.php');
                 else $CRC = 0;
                 
             }
-            echo "<td color='red'> $CRC</td>";
+            echo "<td><p color='red'> $CRC</p></td>";
         }
         echo"</tr></table>";
     }
 
 ?>
+
+<HR width=750>
+
+<form action='' method='get'>
+<table cellpading='4' cellspacing='4' align= center>
+
+
+<tr><h5><b>CRC Polynomial :</b></h5></tr>
+<tr><h6>Saisir le message ainsi que le polynome générateur :</h6></tr>
+<tr>
+<td>Message :</td> 
+<td><input class='champ' name='Message2' type='text' required></td></tr>
+<tr>
+<td>Zero en plus a la fin du message :</td> 
+<td><input class='champ' name='nbZero' type='number' min=0 max=5 required></td></tr>
+<tr>
+<td>Polynome :</td> 
+<td><input class='champ' name='polynome' type='text' required></td></tr>
+
+
+<tr><td align='center' colspan='7'><input name='ConfirmationMessage' type='submit' class='btn btn-success btn-sm' value='Valider'></td></tr>
+</table>
+</form>
+<?php
+    if(isset($_GET['id'])){
+        if($_GET['id']=='2'){
+            echo"<p style='color:red'>message incorrect (que du binaire !) </p>";
+        }
+        if($_GET['id']=='3'){
+            echo"<p style='color:red'>Polynome incorrect (que du binaire !) </p>";
+        }
+        if($_GET['id']=='4'){
+            echo"<p style='color:red'>taille polynome generateur plus grand que le message ! </p>";
+        }
+        if($_GET['id']=='4'){
+            echo"<p style='color:red'>message trop grand (10 bit max !)</p>";
+        }
+    }
+
+    if(isset($_GET['Message2']) and isset($_GET['polynome']) and isset($_GET['nbZero'])){
+
+        $nbZero =$_GET['nbZero'];
+        
+        echo"<h6><b>Le message est : $message2</b></h6>";
+
+        $zero ="";
+        for ($i=0; $i<$nbZero; $i++){
+            $zero.="0";
+        }
+
+        $message2 .= $zero;
+
+        echo"<h6><b>Le message avec $nbZero zero est : $message2</b></h6>";
+
+        echo"<h6><b>Le Polynome générateur est : $polynome</b></h6>";
+
+        $tailleM2 = strlen($message2);
+        
+        while($tailleM2>$taillePol){
+
+            echo "<h6>$message2 => ";
+
+            $debutmessage=substr($message2,0,$taillePol);
+            $finmessage=substr($message2,$taillePol);
+
+            echo "$debutmessage $finmessage => ";
+
+            for($i =0;$i<$taillePol;$i++){
+                if(boolval($debutmessage[$i]) xor boolval($polArray[$i])) $debutmessage[$i]="1";
+                else $debutmessage[$i]="0";
+            }
+
+            echo "$debutmessage $finmessage => ";
+
+            $message2=$debutmessage;
+            $message2.=$finmessage;
+
+            $i=0;
+            while($message2[$i]=="0"){
+                $message2 = substr($message2,1);
+            }
+            
+            $tailleM2= strlen($message2);
+
+            echo "$message2</h6>";
+        }
+
+        
+
+        echo"<h6><b>Le CRC générateur est : $message2</b></h6>";
+        
+        
+    }
+
+
+
+    
+    
+
+?>
+
 
 <footer>
   <HR width=1240>
