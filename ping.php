ffdadb365057faff01d161b2614dadc0c7c902e3
Raffaele G <raffaelejunior@hotmail.fr>
2021-03-14 16:18:08 +0000

    Replace ping.php

--- ping.php
+++ ping.php
@@ -19,8 +19,8 @@ require("barreDeMenu.php");
 <tr>
 <td>nombre de ping</td><td><input type='number' name='nbping' required></td>
 </tr>
-<tr>
-<td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' ></td><td><input name='valider' class="btn btn-danger btn-sm"type='submit' value='Valider' ></td>
+<tr><td></td>
+<td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' ></td>
 </tr>
 
 </table>
