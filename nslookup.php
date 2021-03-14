f6910f9789b7338b303396a0a1ec34d055f01d78
Tom Pierre <pierre.tom@outlook.com>
2021-03-14 17:23:03 +0100

    nslookup

--- nslookup.php
+++ nslookup.php
@@ -4,7 +4,7 @@ require("barreDeMenu.php");
 
 <br/>
 
-<h5 align='center'><p>Sur cette page vous pourrez effectuer un ping sur l'adresse que vous souhaiter.</p></h5>
+<h5 align='center'><p>Sur cette page vous pourrez effectuer un nslookup sur l'adresse que vous souhaiter.</p></h5>
 <HR width=1240>
     <h3 align='center'><b>Formulaire</b></h3>
 <HR width=1240>
@@ -13,7 +13,7 @@ require("barreDeMenu.php");
 <table cellpading='4' cellspacing='4' align='center'>
 
 <tr>
-<td>Adresse a ping</td><td><input type='text' name='nsLookup' required></td>
+<td>Adresse :</td><td><input type='text' name='nsLookup' required></td>
 </tr>
 <tr><td></td>
 <td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' >
