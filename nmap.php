32c98b47aaca13f015ee8f3667c412e63f38e197
Raffaele G <raffaelejunior@hotmail.fr>
2021-03-14 16:17:30 +0000

    Replace nmap.php

--- nmap.php
+++ nmap.php
@@ -16,8 +16,8 @@ require("barreDeMenu.php");
 <td>Adresse a ping</td><td><input type='text' name='nMap' required></td>
 </tr>
 
-<tr>
-<td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' ></td><td><input name='valider' class="btn btn-danger btn-sm"type='submit' value='Valider' ></td>
+<tr><td></td>
+<td><input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' ></td>
 </tr>
 
 </table>
