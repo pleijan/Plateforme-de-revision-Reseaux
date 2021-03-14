cbf92ab30dd6f2739a2a7acc33b14260903d6e0b
Raffaele G <raffaelejunior@hotmail.fr>
2021-03-14 15:04:25 +0000

    Replace bin-dec.php

--- bin-dec.php
+++ bin-dec.php
@@ -19,15 +19,17 @@ require("barreDeMenu.php");
 <form action='bin-dec.php' method='post'>
 <table cellpading='4' cellspacing='4' align='center'>
 
-<tr><td>Adresse IP: </td>
-<td><input class='champ2' name='part1' type='text' value= "<?php if (isset($_POST['part1'])){echo $_POST['part1'];}?>" required> .</td>
-<td><input class='champ2' name='part2' type='text' value= "<?php if (isset($_POST['part2'])){echo $_POST['part2'];}?>" required> .</td>
-<td><input class='champ2' name='part3' type='text' value= "<?php if (isset($_POST['part3'])){echo $_POST['part3'];}?>" required> .</td>
-<td><input class='champ2' name='part4' type='text' value= "<?php if (isset($_POST['part4'])){echo $_POST['part4'];}?>" required> </td></tr></table>
+<tr><td align='right'>Adresse IP :</td> 
+    <td><input class='champ' name='ip' id= "ip" type='text' onfocusin="javascript:nospaces(this)" onfocusout="javascript:nospaces(this)" placeholder="192.168.10.2" value= "<?php if (isset($_POST['ip'])){echo $_POST['ip'];} ?>" required> </td></tr>
+</table>
 
 <table cellpading='4' cellspacing='4' align='center' >
-<tr><td>Binaire -> Décimal <input type='radio' name='trad' value='bintodec' checked></td><td>Décimal -> Binaire<input type='radio' name='trad' value='dectobin'></td></tr>
-<tr></tr>
+<tr>
+    <td>Décimal -> Binaire<input type='radio' name='trad' value='dectobin' checked></td>
+</tr>
+<tr>
+    <td>Binaire -> Décimal <input type='radio' name='trad' value='bintodec'></td>
+</tr>
 </table>
 <input name='valider' class="btn btn-success btn-sm"type='submit' value='Valider' >
 </form>
@@ -39,60 +41,45 @@ function display($a){
     print_r($a);
     echo"</pre>";
 }
-if(isset($_POST['part1'],$_POST['part2'],$_POST['part3'],$_POST['part4'])){
+if(isset($_POST['ip'])){
+    $adrIp = explode(".", $_POST['ip']);
+
     if($_POST['trad']=='dectobin'){
 
-        foreach ($_POST as $val){
-            if ($val == "Valider" or $val == "dectobin" or $val == "bintodec")
+        foreach ($adrIp as $val){
+            if ($val<0 or $val>255)
             {
-
+                header('Location:bin-dec.php?id=1');
             }
-            else {
-                if ($val<0 or $val>255)
-                {
-                    header('Location:bin-dec.php?id=1');
-                }
-                if (!is_numeric($val))
-                {
-                    header('Location:bin-dec.php?id=2');
-                }
-            }   
+            if (!is_numeric($val))
+            {
+                header('Location:bin-dec.php?id=2');
+            }
+              
         }
-        
-        echo"<table cellpading='4' cellspacing='4' align='center' border ='2'>
-        <tr><td>adresse ip original:</td>";
-        foreach($_POST as $val){
-            if ($val == "Valider" or $val == "dectobin" or $val == "bintodec")
-            {
-
-            }
-            else {
-                if ($val == "Valider" or $val == "dectobin")
-                {
-
-                }
-                else
-                {
-                    echo"<td>";
-                    echo $val;
-                    echo"</td>";
-                }
-            }
+        $point = 0;
+        echo"<table cellpading='4' cellspacing='4' align='center'>
+        <tr><td>Adresse IP originale: </td><td>";
+        foreach($adrIp as $val){
+            echo "<td><b>";
+            echo $val;
+            echo "</td></b>";
+             if($point <=2)
+                echo"<td>.</td>";
+            $point++;
         }
-        echo"</tr><tr><td>adresse ip traduite:</td>";
-        foreach($_POST as $val){
-            if ($val == "Valider" or $val == "dectobin")
-            {
-                
-            }
-            else
-            {
-                echo"<td>";
-                echo  decbin($val);
-                echo"</td>";
-            }
+
+        $point = 0;
+        echo"</td></tr><tr><td>Adresse IP traduite: </td><td>";
+        foreach($adrIp as $val){
+            echo "<td><b>";
+            echo  decbin($val);
+            echo "</td></b>";
+             if($point <=2)
+                echo"<td>.</td>";
+            $point++;
         }
-        echo"</tr>";
+        echo"</td></tr>";
     }
 
 
@@ -100,58 +87,43 @@ if(isset($_POST['part1'],$_POST['part2'],$_POST['part3'],$_POST['part4'])){
 
 
     else {
-        echo"<table cellpading='4' cellspacing='4' align='center' border ='2'>
-        <tr><td>adresse ip original:</td>";
+        echo"<table cellpading='4' cellspacing='4' align='center' >
+        <tr><td>Adresse IP originale: </td><td><b>";
         $i = 0;
-        foreach($_POST as $val){
-            if ($val == "Valider" or $val == "dectobin" or $val == "bintodec")
-            {
-
-            }
-            else {
+        $point = 0;
+        foreach($adrIp as $val){
                 
-                $arr0 = str_split($val);
-                foreach ($arr0 as $x) {
-                    if (!($x == 0 || $x == 1)) {
-                        header('Location:bin-dec.php?id=3');
-                    }
-                    if (!is_numeric($x)) {
-                        header('Location:bin-dec.php?id=4');
-                    }
+            $arr0 = str_split($val);
+            foreach ($arr0 as $x) {
+                if (!($x == 0 || $x == 1)) {
+                    header('Location:bin-dec.php?id=3');
                 }
-                if (strlen($val)>8){
-                    header('Location:bin-dec.php?id=5');
+                if (!is_numeric($x)) {
+                    header('Location:bin-dec.php?id=4');
                 }
-               
             }
-
-
-
-            if ($val == "Valider" or $val == "bintodec" or $val == "dectobin")
-            {
-
+            if (strlen($val)>8){
+                header('Location:bin-dec.php?id=5');
             }
-            else
-            {
-                echo"<td>";
-                echo $val;
-                echo"</td>";
-            }
+            echo "<td><b>";
+            echo $val;
+            echo "</td></b>";
+             if($point <=2)
+                echo" <td>.</td>";
+            $point++;
+            
         }
-        echo"</tr><tr><td>adresse ip traduite:</td>";
-        foreach($_POST as $val){
-            if ($val == "Valider" or $val == "bintodec" or $val == "dectobin")
-            {
-                
-            }
-            else
-            {
-                echo"<td>";
-                echo  bindec($val);
-                echo"</td>";
-            }
+        echo"</b></td></tr><tr><td>Adresse IP traduite: </td><td><b>";
+        $point = 0;
+        foreach($adrIp as $val){
+            echo "<td><b>";
+            echo  bindec($val);
+            echo "</td></b>";
+             if($point <=2)
+                echo"<td>.</td>";
+            $point++;
         }
-        echo"</tr>";
+        echo"</b></td></tr>";
     }
 
 }
