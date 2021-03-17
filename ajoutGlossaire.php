30d225c51a1de180c82e4add0203b32a578939c9
Raffaele G <raffaelejunior@hotmail.fr>
2021-03-17 18:42:52 +0000

    Replace ajoutGlossaire.php

--- ajoutGlossaire.php
+++ ajoutGlossaire.php
@@ -13,6 +13,23 @@ if (isset($_SESSION['id'])){
     <HR width=1240>
         <h3 align='center'><b>Formulaire</b></h3>
     <HR width=1240>
+    ";
+    $listeMot =0;
+    if (isset($_GET["listeMot"])) {
+    	$strlisteMot = $_GET["listeMot"];
+    	$listeMot = explode( ',', $strlisteMot);
+    }
+    echo"
+    <table>
+    <div style='float: left;'>
+    	<option value=''><b>Mots déjà entrés :</b></option>
+    	";
+    	foreach ($listeMot as $key => $val) {
+    		echo"<option value='$val'>$val</option>";
+    	}
+    echo"
+    </table
+	</div>
 
     <form action='actionAjoutGloss.php' method='post'>
     <div class='container'>
@@ -23,8 +40,9 @@ if (isset($_SESSION['id'])){
         <div class='input-group'>
             <textarea name='def' class='form-control' placeholder='Entrez sa définition' required></textarea>
         </div>
-        
-      <button type='submit' class = 'btn btn-success btn-sm'>Valider</button>
+      <div class='buttons'>
+      	<button type='submit' class = 'btn btn-success btn-sm'>Valider</button>
+      </div>
     </div>
     <input type='hidden' value='id'/>
     </form>";
